7dcba1a85a65c2db28ee313214a93d695c5a6fe4
Matt Schmulen <matt.schmulen@gmail.com>
2018-11-06 09:03:56 -0500

    move

--- bisect.sh
+++
@@ -1,7 +0,0 @@
-# get it ready
-git bisect start
-git bisect good c09c728
-git bisect bad e6a0692
-
-# give git a command to run against each commit
-git bisect run rspec spec/features/my_broken_spec.rb
